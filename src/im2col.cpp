cb7e4076f362537bf512fd5735e7153adf248666
softcookiepp <softcookiepp@mailfence.com>
2026-06-09 15:29:29 -0700

    welp, up we gooooo

--- src/im2col.cpp
+++ src/im2col.cpp
@@ -162,6 +162,130 @@ static void im2col_out_vk_template(
 	}
 }
 
+void col2im_out_vk_template(
+		Tensor& output,
+		const Tensor& input_,
+		IntArrayRef output_size,
+		IntArrayRef kernel_size,
+		IntArrayRef dilation,
+		IntArrayRef padding,
+		IntArrayRef stride) {
+	TensorArg input_arg{input_, "input", 1};
+	TensorArg output_arg{output, "output", 2};
+	checkAllSameGPU(__func__, {input_arg, output_arg});
+
+	TORCH_CHECK(
+			output_size.size() == 2,
+			"It is expected output_size equals to 2, but got size ",
+			output_size.size());
+
+	TORCH_CHECK(
+			kernel_size.size() == 2,
+			"It is expected kernel_size equals to 2, but got size ",
+			kernel_size.size());
+
+	TORCH_CHECK(
+			dilation.size() == 2,
+			"It is expected dilation equals to 2, but got size ",
+			dilation.size());
+
+	TORCH_CHECK(
+			padding.size() == 2,
+			"It is expected padding equals to 2, but got size ",
+			padding.size());
+
+	TORCH_CHECK(
+			stride.size() == 2,
+			"It is expected stride equals to 2, but got size ",
+			stride.size());
+
+	int64_t output_height = output_size[0];
+	int64_t output_width = output_size[1];
+	int64_t kernel_height = kernel_size[0];
+	int64_t kernel_width = kernel_size[1];
+	int64_t dilation_height = dilation[0];
+	int64_t dilation_width = dilation[1];
+	int64_t pad_height = padding[0];
+	int64_t pad_width = padding[1];
+	int64_t stride_height = stride[0];
+	int64_t stride_width = stride[1];
+
+	at::native::col2im_shape_check(
+		input_,
+		Tensor(),
+		output_height,
+		output_width,
+		kernel_height,
+		kernel_width,
+		dilation_height,
+		dilation_width,
+		pad_height,
+		pad_width,
+		stride_height,
+		stride_width);
+
+	Tensor input = input_.contiguous();
+
+	bool batched_input = true;
+	if (input.dim() == 2) {
+		// Force batch
+		batched_input = false;
+		input = input.unsqueeze(0);
+	}
+
+	int64_t batch_size = input.size(0);
+	int64_t n_input_plane = input.size(1);
+	int64_t n_output_plane = n_input_plane / (kernel_width * kernel_height);
+	int64_t input_batch_stride = input.stride(0);
+
+	output.resize_({batch_size, n_output_plane, output_height, output_width});
+	int64_t output_batch_stride = output.stride(0);
+#if 0
+	AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND3(kHalf, kBFloat16, kBool,
+			input.scalar_type(), "col2im_out_cuda", [&]
+#endif
+	{
+		int64_t height_col = (output_height + 2 * pad_height -
+													(dilation_height * (kernel_height - 1) + 1)) /
+						stride_height +
+				1;
+		int64_t width_col = (output_width + 2 * pad_width -
+												 (dilation_width * (kernel_width - 1) + 1)) /
+						stride_width +
+				1;
+#if 1
+		throw std::runtime_error("col2im_batched not implemented!");
+#else
+		col2im_batched(
+				at::cuda::getCurrentCUDAStream(),
+				input.const_data_ptr<scalar_t>(),
+				input_batch_stride,
+				batch_size,
+				n_output_plane,
+				output_height,
+				output_width,
+				height_col,
+				width_col,
+				kernel_height,
+				kernel_width,
+				pad_height,
+				pad_width,
+				stride_height,
+				stride_width,
+				dilation_height,
+				dilation_width,
+				output.mutable_data_ptr<scalar_t>(),
+				output_batch_stride);
+#endif
+	}
+#if 0
+	);
+#endif
+	if (!batched_input) {
+		output = output.squeeze(0);
+	}
+}
+
 Tensor& im2col_out_vk(const Tensor& input,
 		IntArrayRef kernel_size,
 		IntArrayRef dilation,
@@ -184,6 +308,33 @@ Tensor im2col_vk(const Tensor& input,
 	return output;
 }
 
+Tensor& col2im_out_vk(const Tensor& input,
+		IntArrayRef output_size,
+		IntArrayRef kernel_size,
+		IntArrayRef dilation,
+		IntArrayRef padding,
+		IntArrayRef stride,
+		Tensor& output)
+{
+	col2im_out_vk_template(
+			output, input, output_size, kernel_size, dilation, padding, stride);
+	return output;
+}
+
+Tensor col2im_vk(
+		const Tensor& input,
+		IntArrayRef output_size,
+		IntArrayRef kernel_size,
+		IntArrayRef dilation,
+		IntArrayRef padding,
+		IntArrayRef stride)
+{
+	Tensor output = at::empty_like(input, LEGACY_CONTIGUOUS_MEMORY_FORMAT);
+	col2im_out_vk_template(
+			output, input, output_size, kernel_size, dilation, padding, stride);
+	return output;
+}
+
 } // namespace ptdlprim
 
 TORCH_LIBRARY_IMPL(aten, PrivateUse1, m)
@@ -192,6 +343,8 @@ TORCH_LIBRARY_IMPL(aten, PrivateUse1, m)
 	//m.impl("aten::_slow_conv2d_forward.output", &ptdlprim::slow_conv2d_forward_out_vk);
 	m.impl("aten::im2col", &ptdlprim::im2col_vk);
 	m.impl("aten::im2col.out", &ptdlprim::im2col_out_vk);
+	m.impl("aten::col2im", &ptdlprim::col2im_vk);
+	m.impl("aten::col2im.out", &ptdlprim::col2im_out_vk);
 }
 
 #endif
