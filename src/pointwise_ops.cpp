4150bcf440112f1556d9711c454a90f1a4d39f3b
me <softcookiepp@protonmail.com>
2026-08-29 00:43:47 -0700

    up

--- src/pointwise_ops.cpp
+++ src/pointwise_ops.cpp
@@ -1526,7 +1526,7 @@ using c10::DeviceType;
         Tensor self_c = self.contiguous(), output_c = output.contiguous(), buffer_c = buffer.contiguous();
         dlprim::Tensor x=todp(self_c), out = todp(output_c), buf = todp(buffer_c);
         std::cout << "	before\n";
-        #if 1
+        #if 0
 			// oh wait
 			dlprim::core::pointwiseOpStrided({x}, {out, buf}, {}, dlprim::core::PointwiseOp::eLogSigmoid);
         #else
